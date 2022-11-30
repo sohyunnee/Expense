1b0816e43530e8f6384db63e3022da54bad7f7ab
sohyunnee <98u12@naver.com>
2022-11-30 17:48:45 +0900

    11.30

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
+import './App.css';
+import NewExpense from './components/NewExpense/NewExpense';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -25,7 +27,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses}/>
     </div>
   );
